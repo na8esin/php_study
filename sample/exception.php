cdd5f48ecc9f3c2729ea5ed108e89283d051301c
na8esin <na8esin.mpst@gmail.com>
2020-04-02 13:18:58 +0900

    例外が連鎖する場合

--- sample/exception.php
+++ sample/exception.php
@@ -2,6 +2,9 @@
 try {
     some();
 } catch (\Exception $e) {
+    print_r($e->getMessage());
+    echo PHP_EOL;
+    echo PHP_EOL;
     print_r(json_encode($e->getMessage()));
     echo PHP_EOL;
     echo PHP_EOL;
@@ -9,13 +12,23 @@ try {
     echo PHP_EOL;
     echo PHP_EOL;
     print_r(($e->getTraceAsString()));
-
-    
+    echo PHP_EOL;
+    echo PHP_EOL;
 }
+echo 'external try';
+echo PHP_EOL;
+print_r($e->getMessage());
 
-function some ()
+
+function some()
 {
-    some2();
+    try {
+        some2();
+    } catch (\Exception $e) {
+        throw $e;
+        // こうやると例外が上書きされるけど、いっぺんに2つの例外が表示されることって一般的にないね
+        //throw new \Exception("system down.");
+    }
 }
 
 function some2()
