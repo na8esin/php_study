8c438572c8934d41d727dc9e3d574c6dd7ea6761
na8esin <na8esin.mpst@gmail.com>
2020-06-23 10:11:57 +0900

    不要なファイル削除

--- preg_match_date.php
+++ preg_match_date.php
@@ -1,3 +1,4 @@
 <?php
 var_dump(preg_match('#^\d{4}/\d{2}/\d{2}$#u', '2010/02/01s'));
 var_dump(preg_match('#^\d{4}/\d{2}/\d{2}$#u', '2010/02/01'));
+var_dump(preg_match('#d{4}/d{2}/d{2}#u', '1999/09/09'));
